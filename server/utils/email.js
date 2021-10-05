558e619452af0d26e68bfb6a4451d39b007a95b7
ryma1738 <51891293+ryma1738@users.noreply.github.com>
2021-10-05 16:14:17 -0600

    got email working again!

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,12 +1,14 @@
 const nodemailer = require('nodemailer');
 const mailGun = require('nodemailer-mailgun-transport')
-require('dotenv').config
+require('dotenv').config()
 const auth = {
     auth: {
         api_key: process.env.API_KEY,
         domain: process.env.DOMAIN
-    }
+    },
+    host: 'api.mailgun.net'
 }
+console.log(auth)
 const transporter = nodemailer.createTransport(mailGun(auth));
 
 function sendMail(email, subject, message, cb) {
